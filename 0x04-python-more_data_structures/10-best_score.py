06508d1f2e31d0d2b5474e5d3c60fd22812e8159
MarafaDku <usmanmarafad@gmail.com>
2022-12-07 08:31:19 -0800

    my third 0x04-python-more_data_structures commit

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
-def best_score(my_dict):
-    if my_dict is None or len(my_dict) == 0:
+# 10-best_score.py
+# Usman Marafa D <375@holbertonschool.com>
+
+
+def best_score(a_dictionary):
+    """Returns a key with the biggest integer value."""
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
-    return sorted(my_dict, key=lambda score: score[2])[-1]
+
+    ret = list(a_dictionary.keys())[0]
+    big = a_dictionary[ret]
+    for k, v in a_dictionary.items():
+        if v > big:
+            big = v
+            ret = k
+    return (ret)
